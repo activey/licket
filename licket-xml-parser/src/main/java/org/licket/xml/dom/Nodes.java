5f71f552acc24f05f4ed5c93a50610b8411d0433
activey <activey@doaplatform.org>
2016-09-15 20:55:52 +0200

    Introduced own xml parser in place of XOM, generating proper attribute for *ngFor :) some other stuff introduced ...

--- licket-xml-parser/src/main/java/org/licket/xml/dom/Nodes.java
+++ licket-xml-parser/src/main/java/org/licket/xml/dom/Nodes.java
@@ -1,10 +1,25 @@
 package org.licket.xml.dom;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 /**
  * @author grabslu
  */
-public class Nodes {
-    public Nodes(Node... nodes) {
+public class Nodes extends Node {
 
+    private Node[] nodes = {};
+
+    public Nodes(Node... nodes) {
+        super("", "", "");
+
+        this.nodes = nodes;
+    }
+
+    @Override
+    public void toXML(XMLStreamWriter writer) throws XMLStreamException {
+        for (Node node : nodes) {
+            node.toXML(writer);
+        }
     }
 }
