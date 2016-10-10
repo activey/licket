9d2e2f545e35b5e95ebe52e15613460d18e38105
activey <activey@doaplatform.org>
2016-10-10 19:39:50 +0200

    Indentation done with something else

--- licket-xml-parser/src/main/java/org/licket/xml/dom/Document.java
+++ licket-xml-parser/src/main/java/org/licket/xml/dom/Document.java
@@ -1,15 +1,20 @@
 package org.licket.xml.dom;
 
-import com.sun.xml.internal.txw2.output.IndentingXMLStreamWriter;
+import net.sf.saxon.Configuration;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.Serializer;
 import org.licket.xml.ParsingException;
 
-import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+
+import static net.sf.saxon.s9api.Serializer.Property.INDENT;
+import static net.sf.saxon.s9api.Serializer.Property.METHOD;
 
 /**
  * @author grabslu
@@ -23,7 +28,7 @@ public class Document {
         try {
             XMLStreamWriter streamWriter = streamWriter(writer = new OutputStreamWriter(outputStream));
             rootElement.toXML(streamWriter);
-        } catch (XMLStreamException e) {
+        } catch (XMLStreamException | SaxonApiException e) {
             throw new ParsingException(e);
         } finally {
             if (writer != null) {
@@ -33,8 +38,14 @@ public class Document {
         }
     }
 
-    private XMLStreamWriter streamWriter(Writer writer) throws XMLStreamException {
-        return new IndentingXMLStreamWriter(XMLOutputFactory.newInstance().createXMLStreamWriter(writer));
+    private XMLStreamWriter streamWriter(Writer writer) throws XMLStreamException, SaxonApiException {
+        Configuration config = new Configuration();
+        Processor processor = new Processor(config);
+        Serializer serializer = processor.newSerializer();
+        serializer.setOutputProperty(METHOD, "html");
+        serializer.setOutputProperty(INDENT, "yes");
+        serializer.setOutputWriter(writer);
+        return serializer.getXMLStreamWriter();
     }
 
     public void setRootElement(Element rootElement) {
